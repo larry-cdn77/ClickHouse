e510b49950cbcf51aab299886d992fa65abe6451
Ladislav Snizek <ladislav.snizek@cdn77.com>
2025-09-28 15:33:25 +0200

    Add --no-server-client-version-message and <server_client_version_message/> client option (clean console startup)

--- programs/client/Client.cpp
+++ programs/client/Client.cpp
@@ -323,6 +323,10 @@ void Client::initialize(Poco::Util::Application & self)
     /// Use <warnings/> unless --no-warnings is specified
     if (!config().has("no-warnings") && !config().getBool("warnings", true))
         config().setBool("no-warnings", true);
+
+    /// Use <server_client_version_message/> unless --server-client-version-message is specified
+    if (!config().has("no-server-client-version-message") && !config().getBool("server_client_version_message", true))
+        config().setBool("no-server-client-version-message", true);
 }
 
 
@@ -513,20 +517,23 @@ void Client::connect()
         output_stream << "Connected to " << server_name << " server version " << server_version << "." << std::endl << std::endl;
 
 #if not CLICKHOUSE_CLOUD
-        auto client_version_tuple = std::make_tuple(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
-        auto server_version_tuple = std::make_tuple(server_version_major, server_version_minor, server_version_patch);
+        if (!config().has("no-server-client-version-message"))
+        {
+            auto client_version_tuple = std::make_tuple(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+            auto server_version_tuple = std::make_tuple(server_version_major, server_version_minor, server_version_patch);
 
-        if (client_version_tuple < server_version_tuple)
-        {
-            output_stream << "ClickHouse client version is older than ClickHouse server. "
-                      << "It may lack support for new features." << std::endl
-                      << std::endl;
-        }
-        else if (client_version_tuple > server_version_tuple && server_display_name != "clickhouse-cloud")
-        {
-            output_stream << "ClickHouse server version is older than ClickHouse client. "
-                      << "It may indicate that the server is out of date and can be upgraded." << std::endl
-                      << std::endl;
+            if (client_version_tuple < server_version_tuple)
+            {
+                output_stream << "ClickHouse client version is older than ClickHouse server. "
+                          << "It may lack support for new features." << std::endl
+                          << std::endl;
+            }
+            else if (client_version_tuple > server_version_tuple && server_display_name != "clickhouse-cloud")
+            {
+                output_stream << "ClickHouse server version is older than ClickHouse client. "
+                          << "It may indicate that the server is out of date and can be upgraded." << std::endl
+                          << std::endl;
+            }
         }
 #endif
     }
@@ -685,6 +692,7 @@ void Client::addExtraOptions(OptionsDescription & options_description)
                     "OpenTelemetry tracestate header as described by W3C Trace Context recommendation")
 
                     ("no-warnings", "disable warnings when client connects to server")
+                    ("no-server-client-version-message", "suppress server-client version mismatch message")
         /// TODO: Left for compatibility as it's used in upgrade check, remove after next release and use server setting ignore_drop_queries_probability
         ("fake-drop", "Ignore all DROP queries, should be used only for testing")(
             "accept-invalid-certificate",
@@ -805,6 +813,8 @@ void Client::processOptions(
         config().setBool("compression", options["compression"].as<bool>());
     if (options.count("no-warnings"))
         config().setBool("no-warnings", true);
+    if (options.count("no-server-client-version-message"))
+        config().setBool("no-server-client-version-message", true);
     if (options.count("fake-drop"))
         config().setString("ignore_drop_queries_probability", "1");
     if (options.count("jwt"))
